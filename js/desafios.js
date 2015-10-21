40d7111c9c2b3fbec687d40222b9631d7b40136d
Daniela Salgado <daniela@cameo.mx>
2015-10-20 19:51:47 -0500

    Factorial agregado

--- js/desafios.js
+++ js/desafios.js
@@ -15,7 +15,7 @@ console.log("El resultado de la sumatoria es: " + suma);
 */
 
 // SUMATORIA. Calcular y mostrar número pares del 0 al 100 // 
-
+/*
 var suma = 0;
 
 // Este ciclo nos permite recorrer todos los números del 0 al 100 
@@ -28,3 +28,23 @@ for (var i = 0; i <=100; i += 2) {
 }
 
 console.log("El resultado de la sumatoria es: " + suma);
+*/
+
+// FACTORIAL. Dando un número entero entre 0 y 10, calcula
+
+var resultado = 1; 
+var n = 10; // 5! = 120
+var i = 1;
+
+do {
+	if (n == 0) {
+		break;
+	}
+	// resultados = resultado * i;
+	resultado *= i;
+	i++;
+} while (i <= n); 
+
+console.log("El factorial de " + n + " es " + resultado);
+
+
